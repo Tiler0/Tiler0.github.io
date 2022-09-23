d367eb6e4f57e720ba7ab6b7c91eb113dfa881ee
Tiler0 <Tyler.Price@students.first.edu>
2022-09-22 22:37:27 -0400

    anime

--- animation/assets/js/animation.js
+++ animation/assets/js/animation.js
@@ -1,14 +1,14 @@
 let starter = document.getElementById("starter");
 
 let imgArray = [
-    "assets/imgs/Assets20.png",
-    "assets/imgs/Assets21.png",
-    "assets/imgs/Assets22.png",
-    "assets/imgs/Assets23.png",
-    "assets/imgs/Assets24.png",
-    "assets/imgs/Assets25.png",
-    "assets/imgs/Assets26.png",
-    "assets/imgs/Assets27.png"
+    "assets/imgs/Asset20.png",
+    "assets/imgs/Asset21.png",
+    "assets/imgs/Asset22.png",
+    "assets/imgs/Asset23.png",
+    "assets/imgs/Asset24.png",
+    "assets/imgs/Asset25.png",
+    "assets/imgs/Asset26.png",
+    "assets/imgs/Asset27.png"
 ]
 let count = 0;
 
